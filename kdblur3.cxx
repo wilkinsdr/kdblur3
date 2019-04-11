0c885dd75312f896eee7b449dc865cd33e2fd735
Dan Wilkins <dan.wilkins@stanford.edu>
2019-04-10 17:48:07 -0700

    Implement new binning routine for XSPEC 12.10.1d

--- kdblur3.cxx
+++ kdblur3.cxx
@@ -18,8 +18,9 @@
 #include <XSstreams.h>
 
 // from blurring.cxx
-void blurring(const RealArray& energyArray, const RealArray& fluxArrayKernel,
-	      const Real kernelLineEnergy, RealArray& fluxArray, const bool Reset);
+void blurring(const RealArray& kernelEnergyArray, const RealArray& kernelFluxArray,
+	      const Real kernelLineEnergy, const RealArray& energyArray,
+	      RealArray& fluxArray, const bool Reset);
 
 // from laor3.cxx
 extern "C" void laor3(const RealArray& energyArray, const RealArray& params,
@@ -36,15 +37,29 @@ extern "C" void kdblur3(const RealArray& energyArray, const RealArray& params,
 
   RealArray laor3Params(9);
 
-  // set the laor2 energy to about the middle of the input energy range
+  // set the laor3 energy to 10keV (this is arbitrary)
  
-  laor3Params[0] = energyArray[energyArray.size()/2];
+  laor3Params[0] = 10.0;
   for (size_t i=0; i<8; i++) laor3Params[i+1] = params[i];
 
-  laor3(energyArray, laor3Params, spectrumNumber, kernelFluxArray,
+  // find the minimum binsize in the input energyArray
+  Real minBinsize = energyArray[energyArray.size()-1] - energyArray[0];
+  for (size_t i=0; i<energyArray.size()-1; i++) {
+    Real binsize = energyArray[i+1] - energyArray[i];
+    if ( binsize > 0.0 && binsize < minBinsize ) minBinsize = binsize;
+  }
+
+  // set up the energy array on which the diskline is calculated
+  // from 0 to 20 keV in linear bins of size minBinsize.
+
+  size_t kbins = (size_t)(20.0/minBinsize);
+  RealArray kernelEnergyArray(kbins+1);
+  for (size_t i=0; i<=kbins; i++) kernelEnergyArray[i] = (20.0*i)/((Real)kbins);
+
+  laor3(kernelEnergyArray, laor3Params, spectrumNumber, kernelFluxArray,
        kernelFluxErrArray, initString);
 
-  blurring(energyArray, kernelFluxArray, laor3Params[0], flux, true);
+  blurring(kernelEnergyArray, kernelFluxArray, laor3Params[0], energyArray, flux, true);
 
   return;
   
