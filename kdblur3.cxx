4a03f40b7fa09244f8dbdafa7b29cf41bb06a58c
Dan Wilkins <dan.wilkins@stanford.edu>
2019-08-28 17:54:45 -0400

    New kdblur3 to use XSPEC FFT convolution routine

--- kdblur3.cxx
+++ kdblur3.cxx
@@ -2,65 +2,41 @@
 // disk in the presence of a non-spinning black hole - uses laor3.
 // ACF and RMJ May/June 1998
 // kaa converted to C++ Aug 2018
-// DRW modified kdblur2->kdblur3 October 21, 2018
+// kaa changed to use FFTs Aug 2019
+// DRW version for twice-broken power law emissivity, Aug 2019
 
 //  parameters :
 //       0        power law index for emissivity (10 for disk)
 //       1        inner radius (GM/c**2)
 //       2        outer radius (GM/c**2)
 //       3        inclination  (degrees)
-//       4        break radius (GM/c**2)
-//       5        outer power-law dependence
+//       4        inner break radius (GM/c**2)
+//       5        middle power-law dependence
+//       6        outer break radius (GM/c**2)
+//       7        outer power-law dependence
 
 #include <xsTypes.h>
 #include <functionMap.h>
-#include <XSUtil/Utils/XSutility.h>
-#include <XSstreams.h>
+#include <XSUtil/Numerics/Convolution.h>
 
-// from blurring.cxx
-void blurring(const RealArray& kernelEnergyArray, const RealArray& kernelFluxArray,
-	      const Real kernelLineEnergy, const RealArray& energyArray,
-	      RealArray& fluxArray, const bool Reset);
-
-// from laor3.cxx
 extern "C" void laor3(const RealArray& energyArray, const RealArray& params,
-           int spectrumNumber, RealArray& fluxArray, RealArray& fluxErrArray,
-           const string& initString);
-
+	   int spectrumNumber, RealArray& fluxArray, RealArray& fluxErrArray, 
+	   const string& initString);
 
 extern "C" void kdblur3(const RealArray& energyArray, const RealArray& params,
 	     int spectrumNumber, RealArray& flux, RealArray& fluxErr, 
 	     const string& initString)
 {
 
-  RealArray kernelFluxArray, kernelFluxErrArray;
-
   RealArray laor3Params(9);
 
-  // set the laor3 energy to 10keV (this is arbitrary)
+  // set the laor2 energy to the midpoint of the energy range (this is arbitrary)
  
-  laor3Params[0] = 10.0;
+  laor3Params[0] = (energyArray[energyArray.size()-1]+energyArray[0])/2.0;
   for (size_t i=0; i<8; i++) laor3Params[i+1] = params[i];
 
-  // find the minimum binsize in the input energyArray
-  Real minBinsize = energyArray[energyArray.size()-1] - energyArray[0];
-  for (size_t i=0; i<energyArray.size()-1; i++) {
-    Real binsize = energyArray[i+1] - energyArray[i];
-    if ( binsize > 0.0 && binsize < minBinsize ) minBinsize = binsize;
-  }
-
-  // set up the energy array on which the diskline is calculated
-  // from 0 to 20 keV in linear bins of size minBinsize.
-
-  size_t kbins = (size_t)(20.0/minBinsize);
-  RealArray kernelEnergyArray(kbins+1);
-  for (size_t i=0; i<=kbins; i++) kernelEnergyArray[i] = (20.0*i)/((Real)kbins);
-
-  laor3(kernelEnergyArray, laor3Params, spectrumNumber, kernelFluxArray,
-       kernelFluxErrArray, initString);
-
-  blurring(kernelEnergyArray, kernelFluxArray, laor3Params[0], energyArray, flux, true);
+  Numerics::ConvolutionInLnSpace<laor3>(energyArray, laor3Params, laor3Params[0], 
+				       spectrumNumber, "", flux, fluxErr);
 
   return;
-  
 }
